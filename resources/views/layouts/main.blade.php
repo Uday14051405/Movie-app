3552742b82755321bb321983808fca3e4517f3da
timberlin12 <timberlin743@gmail.com>
2024-11-25 23:54:09 +0530

    Change logo of nav

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,13 +36,13 @@
                     <input type="text" name="" id="" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:ouline-none focus:shadow-outline" placeholder="Search">
                     <div class="absolute top-0">
                         <svg class="mt-1 ml-1" fill="#f0f8ff" width="25px" height="25px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M20.207 18.793L16.6 15.184a7.027 7.027 0 1 0-1.416 1.416l3.609 3.609a1 1 0 0 0 1.414-1.416zM6 11a5 5 0 1 1 5 5 5.006 5.006 0 0 1-5-5z" fill="#fff" style=""></path>
+                            <path d="M20.207 18.793L16.6 15.184a7.027 7.027 0 1 0-1.416 1.416l3.609 3.609a1 1 0 0 0 1.414-1.416zM6 11a5 5 0 1 1 5 5 5.006 5.006 0 0 1-5-5z" fill="#fff"></path>
                         </svg>
                     </div>
                 </div>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="">
-                        <img src="img/avatar.jpg" alt="avtar" class="rounded-full w-8 h-8">
+                        <img src="img/actor5.jpg" alt="avtar" class="rounded-full w-8 h-8">
                     </a>
                 </div>
             </div>
