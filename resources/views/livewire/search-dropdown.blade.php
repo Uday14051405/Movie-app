d1e798941cb686268a8cabed8024d46c108650f7
timberlin12 <timberlin743@gmail.com>
2024-12-01 16:16:25 +0530

    [Add] Models and UI interactive

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,38 +1,53 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.live.debounce.500ms="search" type="text"
-        class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-        placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+    <input
+        wire:model.live.debounce.500ms="search"
+        type="text"
+        class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search (Press '/' to focus)"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
+        @focus="isOpen = true"
+        @keydown="isOpen = true"
+        @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false"
+    >
     <div class="absolute top-0">
-        <svg class="mt-1 ml-1" fill="#f0f8ff" width="25px" height="25px" viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M20.207 18.793L16.6 15.184a7.027 7.027 0 1 0-1.416 1.416l3.609 3.609a1 1 0 0 0 1.414-1.416zM6 11a5 5 0 1 1 5 5 5.006 5.006 0 0 1-5-5z"
-                fill="#fff"></path>
-        </svg>
+        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
 
-    <div wire:loading class="spinner top-1 right-0 mr-4 mt-3"></div>
-    @if(strlen($search) >= 2)
-    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
-        @if($searchResults->count() > 0)
-        <ul>
-            @foreach($searchResults as $result)
-            <li class="border-b border-gray-700">
-                <a href="#" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
-                    @if($result['poster_path'])
-                    <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" srcset=""
-                        class="w-8">
-                    @else
-                    <img src="https://via.placeholde.com/50*75" alt="poster" srcset="" class="w-8">
-                    @endif
-                    <span class="ml-4">{{ $result['title'] }}</span>
-                </a>
-            </li>
-            @endforeach
-        </ul>
-        @else
-        <div class="px-3 py-3">No results for "{{ $search}}"</div>
-        @endif
-    </div>
+    <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
+
+    @if (strlen($search) >= 2)
+        <div
+            class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4"
+            x-show.transition.opacity="isOpen"
+        >
+            @if ($searchResults->count() > 0)
+                <ul>
+                    @foreach ($searchResults as $result)
+                        <li class="border-b border-gray-700">
+                            <a
+                                href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+                                @if ($loop->last) @keydown.tab="isOpen = false" @endif
+                            >
+                            @if ($result['poster_path'])
+                                <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
+                            @else
+                                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                            @endif
+                            <span class="ml-4">{{ $result['title'] }}</span>
+                        </a>
+                        </li>
+                    @endforeach
+
+                </ul>
+            @else
+                <div class="px-3 py-3">No results for "{{ $search }}"</div>
+            @endif
+        </div>
     @endif
 </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,14 +4,21 @@
 
 <div class="movie-info border-b border-gray-800">
     <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-        <img src="{{'https://image.tmdb.org/t/p/w300/' . $movie['poster_path']}}" alt="parasite" class="w-64 lg:w-96">
+        <div class="flex-none">
+            <img src="{{'https://image.tmdb.org/t/p/w300/' . $movie['poster_path']}}" alt="" class="w-64 lg:w-96">
+        </div>
         <div class="md:ml-24">
             <h2 class="text-4xl font-semibold">{{$movie['title']}}</h2>
             <div class="flex flex-wrap items-center text-gray-400 text-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                    width="16" height="16" viewBox="0 0 256 256" xml:space="preserve">
                     <defs></defs>
-                    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-                        <path d="M 45.002 75.502 c 2.862 0 5.72 0.684 8.326 2.051 l 19.485 10.243 l -3.721 -21.678 c -1.002 -5.815 0.926 -11.753 5.164 -15.877 L 90 34.895 l -21.768 -3.161 c -5.838 -0.85 -10.884 -4.514 -13.499 -9.806 L 44.998 2.205 l -9.73 19.717 c -2.615 5.292 -7.661 8.962 -13.499 9.811 L 0 34.895 L 15.749 50.25 c 4.224 4.111 6.156 10.044 5.16 15.863 l -3.721 21.682 l 19.466 -10.238 C 39.268 76.19 42.135 75.502 45.002 75.502 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,207,100); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;"
+                        transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                        <path
+                            d="M 45.002 75.502 c 2.862 0 5.72 0.684 8.326 2.051 l 19.485 10.243 l -3.721 -21.678 c -1.002 -5.815 0.926 -11.753 5.164 -15.877 L 90 34.895 l -21.768 -3.161 c -5.838 -0.85 -10.884 -4.514 -13.499 -9.806 L 44.998 2.205 l -9.73 19.717 c -2.615 5.292 -7.661 8.962 -13.499 9.811 L 0 34.895 L 15.749 50.25 c 4.224 4.111 6.156 10.044 5.16 15.863 l -3.721 21.682 l 19.466 -10.238 C 39.268 76.19 42.135 75.502 45.002 75.502 z"
+                            style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,207,100); fill-rule: nonzero; opacity: 1;"
+                            transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
                     </g>
                 </svg>
                 <span class="ml-1">{{$movie['vote_average'] * 10}}%</span>
@@ -42,16 +49,42 @@
                 </div>
             </div>
 
-            @if (count($movie['videos']['results']) > 0)
-                <div class="mt-12">
-                    <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key']}}" class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-white hover:bg-orange-600 transition ease-in-out duration-150">
-                        <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 512">
-                            <path fill-rule="nonzero" d="M255.99 0c70.68 0 134.7 28.66 181.02 74.98C483.33 121.3 512 185.31 512 256c0 70.68-28.67 134.69-74.99 181.01C390.69 483.33 326.67 512 255.99 512S121.3 483.33 74.98 437.01C28.66 390.69 0 326.68 0 256c0-70.67 28.66-134.7 74.98-181.02C121.3 28.66 185.31 0 255.99 0zm77.4 269.81c13.75-8.88 13.7-18.77 0-26.63l-110.27-76.77c-11.19-7.04-22.89-2.9-22.58 11.72l.44 154.47c.96 15.86 10.02 20.21 23.37 12.87l109.04-75.66zm79.35-170.56c-40.1-40.1-95.54-64.92-156.75-64.92-61.21 0-116.63 24.82-156.74 64.92-40.1 40.11-64.92 95.54-64.92 156.75 0 61.22 24.82 116.64 64.92 156.74 40.11 40.11 95.53 64.93 156.74 64.93 61.21 0 116.65-24.82 156.75-64.93 40.11-40.1 64.93-95.52 64.93-156.74 0-61.22-24.82-116.64-64.93-156.75z" />
-                        </svg>
-                        <span class="ml-2">Play Trailer</span>
-                    </a>
-                </div>
-            @endif
+            <div x-data="{ isOpen: false }">
+                @if (count($movie['videos']['results']) > 0)
+                    <div class="mt-12">
+                        <button
+                            @click="isOpen = true"
+                            class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150"
+                        >
+                            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                            <span class="ml-2">Play Trailer</span>
+                        </button>
+                    </div>
+                    <template x-if="isOpen">
+                        <div
+                            style="background-color: rgba(0, 0, 0, .5);"
+                            class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+                        >
+                            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                                <div class="bg-gray-900 rounded">
+                                    <div class="flex justify-end pr-4 pt-2">
+                                        <button
+                                            @click="isOpen = false"
+                                            @keydown.escape.window="isOpen = false"
+                                            class="text-3xl leading-none hover:text-gray-300">&times;
+                                        </button>
+                                    </div>
+                                    <div class="modal-body px-8 py-8">
+                                        <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                                            <iframe class="responsive-iframe absolute top-0 left-0 w-full h-full" src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border:0;" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                @endif
+            </div>
         </div>
     </div>
 </div>
@@ -59,7 +92,7 @@
 <div class="movie-cast border-b border-gray-800">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Cast</h2>
-        <div class="grid grid-cols-1 sm:grid-cls-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
             @foreach ($movie['credits']['cast'] as $cast)
                 @if ($loop->index < 5)
                     <div class="mt-8">
@@ -67,7 +100,7 @@
                             <img src="{{'https://image.tmdb.org/t/p/w200/' . $cast['profile_path']}}" alt="parasite" class="w-64 lg:w-96">
                         </a>
                         <div class="mt-2">
-                            <a href="#" class="text-lg mt-2 hover: text-gray:300">{{$cast['name']}}</a>
+                            <a href="#" class="text-lg mt-2 hover:text-gray-300">{{$cast['name']}}</a>
                             <div class="text-gray-400 text-sm">
                                 {{$cast['character']}}
                             </div>
@@ -75,23 +108,43 @@
                     </div>
                 @endif
             @endforeach
-            
-            
         </div>
     </div>
 </div>
 
-<div class="movie-images">
+<div class="movie-images" x-data="{ isOpen: false, image: ''}">
     <div class="container mx-auto px-4 py-16">
         <h2 class="text-4xl font-semibold">Images</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-            @foreach ($movie['images']['backdrops'] as $image)
-                @if ($loop->index < 9)
+        @foreach ($movie['images']['backdrops'] as $image)
                     <div class="mt-8">
-                        <img src="{{'https://image.tmdb.org/t/p/w500/' . $image['file_path']}}" alt="parasite" class="w-full">
+                        <a
+                            @click.prevent="
+                                isOpen = true
+                                image='{{ 'https://image.tmdb.org/t/p/original/'.$image['file_path'] }}'
+                            "
+                            href="#"
+                        >
+                            <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="image1" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
                     </div>
-                @endif
-            @endforeach
+                @endforeach
+        </div>
+        <div style="background-color: rgba(0, 0, 0, .5);" class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto" x-show="isOpen">
+            <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+                <div class="bg-gray-900 rounded">
+                    <div class="flex justify-end pr-4 pt-2">
+                        <button
+                            @click="isOpen = false"
+                            @keydown.escape.window="isOpen = false"
+                            class="text-3xl leading-none hover:text-gray-300">&times;
+                        </button>
+                    </div>
+                    <div class="modal-body px-8 py-8">
+                        <img :src="image" alt="poster">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
